04abe876f7c30362e598572147f8790b2e59bd8d
Chelsea <chelsea.dommert@gmail.com>
2014-12-01 08:42:18 -0600

    Given length of pendulum, calculates period

--- physics.go
+++ physics.go
@@ -1,6 +1,32 @@
 package physics
 
-//calculates force in Newtons given a mass in kg and an acceleration in m/(s^2)
+import "math"
+
+const(
+  g = 9.80
+  // pi  = 3.14159265358979323846264338327950288419716939937510582097494459
+)
+
 func Force(mass float64, acceleration float64) float64{
   return mass * acceleration
+}
+
+func PendulumPeriod(length float64) float64{
+  return round(2 * math.Pi * math.Sqrt(length/g), 0.5, 3)
+}
+
+func round(val float64, roundOn float64, places int ) (newVal float64) {
+  var round float64
+  pow := math.Pow(10, float64(places))
+  digit := pow * val
+  _, div := math.Modf(digit)
+  _div := math.Copysign(div, val)
+  _roundOn := math.Copysign(roundOn, val)
+  if _div >= _roundOn {
+    round = math.Ceil(digit)
+  } else {
+    round = math.Floor(digit)
+  }
+  newVal = round / pow
+  return
 }
--- physics_test.go
+++ physics_test.go
@@ -8,10 +8,18 @@ import (
 )
 
 var _ = Describe("Physics", func() {
-  Describe("Calculates Force", func() {
+  Describe("Force", func() {
         Context("With mass and acceleration", func() {
             It("should calculate force", func() {
                 Expect(Force(2.5, 4.0)).To(Equal(10.0))
+            })
+        })     
+  })
+
+   Describe("Pendulums", func() {
+        Context("With length of pendulum", func() {
+            It("should calculate period", func() {
+                Expect(PendulumPeriod(1.0)).To(Equal(2.007))
             })
         })     
   })
